bca6ba434aed5e80feedd8e5fb7946f7f3cc3b49
Prem Naga Siva <premnagasiva@gmail.com>
2026-01-04 19:50:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
-const item = document.getElementbyId('level')
-let level = 0
-let parent = item
+const item = document.getElementbyId('level');
+let level = 0;
+let parent = item;
 
 while(parent){
 	level++
-	parent.parentElement()
+	parent = parent.parentElement()
 }
 alert(`The level of the element is: ${level}`)
